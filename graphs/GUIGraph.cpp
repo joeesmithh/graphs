82dde8669393e88328146a858f0d51d9022e191a
joeesmithh <josmth3030@gmail.com>
2025-09-13 04:36:18 -0400

    refactor: cleanup code and add comments for clarity

--- graphs/GUIGraph.cpp
+++ graphs/GUIGraph.cpp
@@ -7,53 +7,24 @@ GUIGraph::GUIGraph(QWidget* parent)
 	view = new QGraphicsView(scene, parent);
 	view->setScene(scene);
 	view->setRenderHint(QPainter::RenderHint::TextAntialiasing);
-	
-	//populateGraph(50);
-
-	/*auto func = [](GUIVertex* vertex) {
-		vertex->setPen(QPen(QColor(0, 0, 255)));
-	};*/
 }
 
-QGraphicsView* GUIGraph::getView()
-{
-	return view;
-}
+QGraphicsView* GUIGraph::getView() { return view; }
 
 void GUIGraph::addVertex(const int& xPos, const int& yPos,
 	const QString& label, const QString& label_connectTo)
 {
+	// Initialize dummy vertex for search key
 	auto newVertex = std::make_shared<GUIVertex>(0, 0, 30, 30, label);
+
+	// Initialize new vertex to connect to, if needed
 	auto connectVertex = std::make_shared<GUIVertex>(xPos, yPos, 30, 30, label_connectTo);
+
+	// Connect the two vertices based on key values
 	vertices.add(*newVertex, *connectVertex, [this](GUIVertex& vert) {
+
+		// Initialize newly created vertices in scene
 		vert.display();
 		scene->addItem(vert.getEllipse());
-		}, 
-		1);
-	std::cout << "Size: " << vertices.getNumVertices() << std::endl;
-}
-
-void GUIGraph::populateGraph(const int& vertices)
-{
-	/* Create ellipses */
-	//GUIVertex* previous = nullptr;
-	//for (int i = 1, j = 1; i <= vertices; i++)
-	//{
-	//	/* Accumulate radius multiplier */
-	//	if (((2 + i) % 3) == 0) {
-	//		j++;
-	//	}
-
-	//	GUIVertex* vertex = new GUIVertex((j * VERTEX_SPACING) * -cos(i), (j * VERTEX_SPACING) * -sin(i),
-	//		VERTEX_SCALE, VERTEX_SCALE,
-	//		QString::number(i));
-
-	//	if (previous != nullptr)
-	//		GUIGraph::vertices.add(previous, vertex, 1);
-
-	//	/* Create and add ellipse to scene */
-	//	scene()->addItem(vertex);
-
-	//	previous = vertex;
-	//}
+		}, 1);
 }
--- graphs/GUIGraph.h
+++ graphs/GUIGraph.h
@@ -3,27 +3,35 @@
 #include "ListGraph.h"
 #include "GUIVertex.h"
 
+/** GUI representation of an adjacenecy list graph. */
 class GUIGraph : public QObject {
 
 	Q_OBJECT
 
 public:
+	/** Construct a graph with a parent.
+		@param parent The QWidget with which to form a parent-child relatioship. */
 	GUIGraph(QWidget* parent = nullptr);
+
+	/** Retrieve the graph's graphics view.
+		@returns a pointe to the QtGraphicsView associated with
+			the graph */
 	QGraphicsView* getView();
 	
-
 private:
-	int VERTEX_SCALE = 32;
-	int VERTEX_SPACING = 24;
-
 	QGraphicsView* view;
 	QGraphicsScene* scene;
 	ListGraph<GUIVertex> vertices;
 
-	void populateGraph(const int& vertices);
-
 public slots:
-	void addVertex(const int& cPos, const int& yPos,
+	/** GUIVertex creation event handler. Adds new or connects existing vertices.
+		@param xPos Horizontal scene position
+		@param yPos Vertical scene position
+		@param label The label displayed with the origin vertex, also used for
+			comparison operations.
+		@param label_connectTo The label displayed with the destination vertex,
+			also used for comparison operations. */
+	void addVertex(const int& xPos, const int& yPos,
 		const QString& label, const QString& label_connectTo);
 
 };
